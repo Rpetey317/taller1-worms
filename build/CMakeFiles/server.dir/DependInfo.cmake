f240ca1e6f4e0fd1b69f4cdfeb84ffd852a16995
Mateo Lardiez <mlardiez@fi.uba.ar>
2023-10-29 21:39:24 -0300

    changing path names

---
+++ build/CMakeFiles/server.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/mateolardiez/Facultad/Taller1/tpWorms/taller1-worms/common/Socket/LibError.cpp" "CMakeFiles/server.dir/common/Socket/LibError.cpp.o" "gcc" "CMakeFiles/server.dir/common/Socket/LibError.cpp.o.d"
+  "/home/mateolardiez/Facultad/Taller1/tpWorms/taller1-worms/common/Socket/Resolver.cpp" "CMakeFiles/server.dir/common/Socket/Resolver.cpp.o" "gcc" "CMakeFiles/server.dir/common/Socket/Resolver.cpp.o.d"
+  "/home/mateolardiez/Facultad/Taller1/tpWorms/taller1-worms/common/Socket/ResolverError.cpp" "CMakeFiles/server.dir/common/Socket/ResolverError.cpp.o" "gcc" "CMakeFiles/server.dir/common/Socket/ResolverError.cpp.o.d"
+  "/home/mateolardiez/Facultad/Taller1/tpWorms/taller1-worms/common/Socket/Socket.cpp" "CMakeFiles/server.dir/common/Socket/Socket.cpp.o" "gcc" "CMakeFiles/server.dir/common/Socket/Socket.cpp.o.d"
+  "/home/mateolardiez/Facultad/Taller1/tpWorms/taller1-worms/server/AccepterThread/AccepterThread.cpp" "CMakeFiles/server.dir/server/AccepterThread/AccepterThread.cpp.o" "gcc" "CMakeFiles/server.dir/server/AccepterThread/AccepterThread.cpp.o.d"
+  "/home/mateolardiez/Facultad/Taller1/tpWorms/taller1-worms/server/ClientUpdate/ClientUpdate.cpp" "CMakeFiles/server.dir/server/ClientUpdate/ClientUpdate.cpp.o" "gcc" "CMakeFiles/server.dir/server/ClientUpdate/ClientUpdate.cpp.o.d"
+  "/home/mateolardiez/Facultad/Taller1/tpWorms/taller1-worms/server/Game/Game.cpp" "CMakeFiles/server.dir/server/Game/Game.cpp.o" "gcc" "CMakeFiles/server.dir/server/Game/Game.cpp.o.d"
+  "/home/mateolardiez/Facultad/Taller1/tpWorms/taller1-worms/server/GameLoopThread/GameLoopThread.cpp" "CMakeFiles/server.dir/server/GameLoopThread/GameLoopThread.cpp.o" "gcc" "CMakeFiles/server.dir/server/GameLoopThread/GameLoopThread.cpp.o.d"
+  "/home/mateolardiez/Facultad/Taller1/tpWorms/taller1-worms/server/GameUpdate/GameUpdate.cpp" "CMakeFiles/server.dir/server/GameUpdate/GameUpdate.cpp.o" "gcc" "CMakeFiles/server.dir/server/GameUpdate/GameUpdate.cpp.o.d"
+  "/home/mateolardiez/Facultad/Taller1/tpWorms/taller1-worms/server/PlayerHandler/PlayerHandler.cpp" "CMakeFiles/server.dir/server/PlayerHandler/PlayerHandler.cpp.o" "gcc" "CMakeFiles/server.dir/server/PlayerHandler/PlayerHandler.cpp.o.d"
+  "/home/mateolardiez/Facultad/Taller1/tpWorms/taller1-worms/server/PlayerListMonitor/PlayerListMonitor.cpp" "CMakeFiles/server.dir/server/PlayerListMonitor/PlayerListMonitor.cpp.o" "gcc" "CMakeFiles/server.dir/server/PlayerListMonitor/PlayerListMonitor.cpp.o.d"
+  "/home/mateolardiez/Facultad/Taller1/tpWorms/taller1-worms/server/ReceiverThread/ReceiverThread.cpp" "CMakeFiles/server.dir/server/ReceiverThread/ReceiverThread.cpp.o" "gcc" "CMakeFiles/server.dir/server/ReceiverThread/ReceiverThread.cpp.o.d"
+  "/home/mateolardiez/Facultad/Taller1/tpWorms/taller1-worms/server/SenderThread/SenderThread.cpp" "CMakeFiles/server.dir/server/SenderThread/SenderThread.cpp.o" "gcc" "CMakeFiles/server.dir/server/SenderThread/SenderThread.cpp.o.d"
+  "/home/mateolardiez/Facultad/Taller1/tpWorms/taller1-worms/server/ServerProtocol/ServerProtocol.cpp" "CMakeFiles/server.dir/server/ServerProtocol/ServerProtocol.cpp.o" "gcc" "CMakeFiles/server.dir/server/ServerProtocol/ServerProtocol.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
