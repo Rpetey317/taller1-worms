f28e3c9e1b35a9aa9d59c7ec364cc89353eeb827
Mateo Lardiez <mlardiez@fi.uba.ar>
2023-11-21 10:57:30 -0300

    trying to fix segFalut error but could not. Adding prints to know where the error is. Creatin file to document ClientMessages

--- Server/src/ServerProtocol/ServerProtocol_sendUpdate.cpp
+++ Server/src/ServerProtocol/ServerProtocol_sendUpdate.cpp
@@ -68,6 +68,7 @@ char ServerProtocol::send_PlayerDisconnectedUpdate(const GamePlayerDisconnectedU
 
 char ServerProtocol::send_PlayerConnectedUpdate(const GamePlayerConnectedUpdate& upd) {
     // send code
+    std::cout << "Sending player connected" << std::endl; // Nunca se imprime. No llega a esta funcion
     if (!this->send_char(MSGCODE_PLAYER_CONNECT)) {
         return CLOSED_SKT;
     }
@@ -76,6 +77,8 @@ char ServerProtocol::send_PlayerConnectedUpdate(const GamePlayerConnectedUpdate&
     if (!this->send_char((playerid_t)upd.get_player_id())) {
         return CLOSED_SKT;
     }
+    std::cout << "Sent player connected" << std::endl;
+
     return SUCCESS;
 }
 
