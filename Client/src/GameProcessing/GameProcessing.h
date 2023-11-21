e39774dc7f69940f47a5e0dcb6d8661106215feb
Mateo Lardiez <mlardiez@fi.uba.ar>
2023-11-21 12:32:10 -0300

    adding client documentation

--- Client/src/GameProcessing/GameProcessing.h
+++ Client/src/GameProcessing/GameProcessing.h
@@ -19,23 +19,42 @@
 class GameProcessing {
 private:
     Socket skt;
-    ClientProtocol protocol;  // El thread receiver y sender deberian tener el clientProtocol?
-    Queue<Action*>& outgoingq;
-    Queue<Event*>& incomingq;
+    ClientProtocol protocol;
+    Queue<Action*>& outgoingq; // For sender thread. SDL has a reference too
+    Queue<Event*>& incomingq; // For receiver thread. SDL has a reference too
     ReceiverThread receiverTh;
     SenderThread senderTh;
     EventProcessor eventProcessor;
     int id;
 
 public:
-    // explicit GameProcessing(const char* hostname, const char* port);
+    /*
+        Creates a GameProcessing object. It will create a socket and connect to the server.
+        It will also create the threads for sending and receiving messages.
+    */
     explicit GameProcessing(const char* hostname, const char* port, Queue<Action*>& commands,
                             Queue<Event*>& events);
 
+    /*
+        Checks if the command send by the user in the terminal is valid.
+    */
     std::string ask_for_command();
-    void alternate_run();
+
+    void alternate_run(); // Should delete
+
+    /*
+        Runs the game. Receives the id of the client and runs the receiver and sender threads
+    */
     void run();
+
+    /*
+        Returns the id of the client
+    */
     int get_id();
+    
+    /*
+        Ends the game. Joins the threads.
+    */
     void end();
 
     ~GameProcessing();
